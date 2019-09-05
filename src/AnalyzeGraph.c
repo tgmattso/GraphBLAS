a0a8f82cd118fb436b834b3d5cc94442b2674ddb
Scott McMillan <smcmillan@sei.cmu.edu>
2019-09-04 23:35:53 -0400

    Adding steps to extract the largest cluster from the complete graph, perform pagerank on the extracted component and output the highest ranked author

--- src/AnalyzeGraph.c
+++ src/AnalyzeGraph.c
@@ -48,9 +48,19 @@
 GrB_Info CountCC(GrB_Vector parents, GrB_Index* countcc);
 
 //------------------------------------------------------------------------------
+GrB_Index G_target_id = 0;
+
+void eq_target(void *z, const void *x)
+{
+    (*(bool*)z) = (bool)((*(GrB_Index*)x) == G_target_id);
+    //printf("target %ld : element %ld => %d\n", G_target_id, (*(GrB_Index*)x),
+    //       (*(bool*)z));
+}
+
+//------------------------------------------------------------------------------
 // BFS visited: (from matvecTransIterVisitedExitFlag.c)
 //------------------------------------------------------------------------------
-GrB_Info BFS_mark(GrB_Matrix const A, GrB_Index src_node, GrB_Vector *v)
+GrB_Info BFS_mark(GrB_Matrix const A, GrB_Index src_node, GrB_Vector v)
 {
     GrB_Index n;
     GrB_Matrix_nrows(&n, A);
@@ -68,8 +78,8 @@ GrB_Info BFS_mark(GrB_Matrix const A, GrB_Index src_node, GrB_Vector *v)
 
     do
     {
-        GrB_eWiseAdd(*v, GrB_NULL, GrB_NULL, GrB_LOR, *v, w, GrB_NULL);
-        GrB_vxm(w, *v, GrB_NULL, GxB_LOR_LAND_BOOL, w, A, desc);
+        GrB_eWiseAdd(v, GrB_NULL, GrB_NULL, GrB_LOR, v, w, GrB_NULL);
+        GrB_vxm(w, v, GrB_NULL, GxB_LOR_LAND_BOOL, w, A, desc);
         GrB_Vector_nvals(&nvals, w);
     } while (nvals > 0);
 
@@ -100,7 +110,7 @@ GrB_Info CC_iterative(GrB_Matrix const  A,
         if (GrB_NO_VALUE == GrB_Vector_extractElement(&c_num, *components, src_node))
         {
             // Traverse as far as you can go from src_node marking all visited nodes
-            BFS_mark(A, src_node, &v);
+            BFS_mark(A, src_node, v);
 
             // Merge visited list into components (give component source node name)
             // components[v] = src_node
@@ -166,7 +176,7 @@ int main (int argc, char **argv)
     GrB_Vector_new(&degree, GrB_UINT64, num_rows);
     GrB_reduce(degree, GrB_NULL, GrB_NULL, GrB_PLUS_UINT64, A, GrB_NULL);
 
-    GrB_Index max_val, max_index;
+    GrB_Index max_val, target_index;
     for (GrB_Index ix = 0; ix < num_rows; ++ix)
     {
         GrB_Index val;
@@ -175,14 +185,15 @@ int main (int argc, char **argv)
             if (val > max_val)
             {
                 max_val   = val;
-                max_index = ix;
+                target_index = ix;
             }
         }
     }
 
-    printf("Author with the most coauthor/paper combos: %ld (count: %ld)\n", max_index, max_val);
+    printf("Author with the most coauthor/paper combos: %ld (count: %ld)\n", target_index, max_val);
 
-    // Use LACC connected components algorithm
+    //============================================================
+    // Use LAGraph's LACC connected components algorithm
     printf("#1: Running LAGraphs LACC algorithm\n");
     GrB_Vector LACC_result;
     LAGraph_tic (tic);
@@ -197,11 +208,12 @@ int main (int argc, char **argv)
 
     //pretty_print_vector_UINT64(LACC_result, "Connected components #1");
 
-    GrB_Index max_component = 666;
-    GrB_Vector_extractElement(&max_component, LACC_result, max_index);
-    printf("Component for author %ld: %ld\n", max_index, max_component);
+    GrB_Index cluster_num = 666;
+    GrB_Vector_extractElement(&cluster_num, LACC_result, target_index);
+    printf("Component for author %ld: %ld\n", target_index, cluster_num);
 
-    // Run the CC algorithm above
+    //============================================================
+    // Run the class generated CC algorithm above
     printf("#2: Running naive CC algorithm\n");
     GrB_Vector components;
     GrB_Index  num_comps;
@@ -214,9 +226,83 @@ int main (int argc, char **argv)
 
     //pretty_print_vector_UINT64(components, "Connected components #2");
 
-    GrB_Vector_extractElement(&max_component, components, max_index);
-    printf("Component for author %ld: %ld\n", max_index, max_component);
+    GrB_Vector_extractElement(&cluster_num, components, target_index);
+    printf("Component for author %ld: %ld\n", target_index, cluster_num);
 
+    //===========================================================
+    // Find all the elements in the target cluster
+    G_target_id = cluster_num;
+    GrB_UnaryOp target_eq = NULL;
+    GrB_UnaryOp_new(&target_eq, &eq_target, GrB_BOOL, GrB_UINT64);
+
+    GrB_Vector cluster_mask;
+    GrB_Vector_new(&cluster_mask, GrB_BOOL, num_rows);
+
+    // Find all elements belonging to cluster labeled cluster_num
+    GrB_apply(cluster_mask, GrB_NULL, GrB_NULL,
+              target_eq, components, GrB_NULL);
+
+    GrB_Index nc;
+    GrB_Vector_nvals(&nc, cluster_mask);
+    printf("Cluster mask nvals (before masking): %ld\n", nc);
+
+    // remove all elements that have 'stored false'
+    GrB_Descriptor desc;
+    GrB_Descriptor_new(&desc);
+    GrB_Descriptor_set(desc, GrB_OUTP, GrB_REPLACE);
+
+    GrB_apply(cluster_mask, cluster_mask, GrB_NULL,
+              GrB_IDENTITY_BOOL, cluster_mask, desc);
+
+    GrB_Vector_nvals(&nc, cluster_mask);
+    printf("Cluster mask nvals (after masking): %ld\n", nc);
+
+    // get number of elements in the mask and extract the indices
+    GrB_Index component_size;
+    GrB_Vector_nvals(&component_size, cluster_mask);
+    printf("Component size: %ld\n", component_size);
+    GrB_Index *cluster_indices = malloc(component_size*sizeof(GrB_Index));
+    bool      *vals = malloc(component_size*sizeof(bool));
+
+    GrB_Vector_extractTuples(cluster_indices, vals,
+                             &component_size, cluster_mask);
+    printf("Extracted component size: %ld\n", component_size);
+
+    //===========================================================
+    // extract the component from the rest of the graph
+    GrB_Matrix A_comp;
+    GrB_Matrix_new(&A_comp, GrB_UINT64, component_size, component_size);
+    GrB_extract(A_comp, GrB_NULL, GrB_NULL, A,
+                cluster_indices, component_size,
+                cluster_indices, component_size,
+                GrB_NULL);
+
+    GrB_Vector pr;
+    double max_rank = 0.0;
+    GrB_Index max_rank_id;
+    LAGraph_pagerank2(&pr, A_comp, 0.85, 100);
+    printf("ID:\tRank\n");
+    for (GrB_Index ix = 0; ix < component_size; ++ix)
+    {
+        double rank=-1.;
+        GrB_Vector_extractElement(&rank, pr, ix);
+        printf("%ld\t%lf\n", cluster_indices[ix], rank);
+        if (rank > max_rank)
+        {
+            max_rank = rank;
+            max_rank_id = cluster_indices[ix];
+        }
+    }
+
+    printf("Author with the highest rank: %ld (%lf)\n", max_rank_id, max_rank);
+
+    //===========================================================
+    free(cluster_indices);
+    free(vals);
+    GrB_free(&cluster_mask);
+    GrB_free(&target_eq);
+    GrB_free(&LACC_result);
+    GrB_free(&components);
     GrB_free(&A);
     LAGraph_finalize ( ) ;
     return (GrB_SUCCESS) ;
